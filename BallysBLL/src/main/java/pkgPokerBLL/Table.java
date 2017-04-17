7899eb79ca44ac49526a10841a5c09e1dbd85b11
brock <brock@128.4.68.40>
2017-04-17 16:40:14 -0400

    Table Hashmap complete

--- BallysBLL/src/main/java/pkgPokerBLL/Table.java
+++ BallysBLL/src/main/java/pkgPokerBLL/Table.java
@@ -1,15 +1,14 @@
+
 package pkgPokerBLL;
 
-import java.io.Serializable;
-import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.UUID;
 
-public class Table implements Serializable {
+public class Table {
 
 	private UUID TableID;
 	
-	//	Change this from ArrayList to HashMap.
-	private ArrayList<Player> TablePlayers = new ArrayList<Player>();
+	private HashMap<UUID, Player > TablePlayers = new HashMap<UUID,Player>();
 	
 	public Table() {
 		super();
@@ -18,13 +17,15 @@ public class Table implements Serializable {
 	
 	public Table AddPlayerToTable(Player p)
 	{
-		//TODO: Add a player to the table (should be pretty easy)		
+				
+		TablePlayers.put(p.getPlayerID(), p);
 		return this;
 	}
 	
 	public Table RemovePlayerFromTable(Player p)
 	{
-		//TODO: Remove a player from the table
+		
+		TablePlayers.remove(p);
 		return this;
 	}
 }
